6ec85440c8ef6d0282c6a59960e6b397017ded4a
slnluis <81486233+slnluis@users.noreply.github.com>
2022-10-13 17:01:35 -0600

    Update bookingForm.php

--- php/bookingForm.php
+++ php/bookingForm.php
@@ -9,13 +9,20 @@ $date   = $_REQUEST["date"];
 $to    = "positivwellness.info@gmail.com"; // ENTER YOUR EMAIL ADDRESS
 if (isset($email) && isset($firstname) && isset($lastname)) {
     $email_subject = "$name a enviado un mensaje via Positiv Wellness Center"; // ENTER YOUR EMAIL SUBJECT
-		$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-$headers .= "De: ".$firstname." ".$lastname." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
-$msg     = "Servicio: $service <br/> Staff: $staff <br/> Fecha solicitada: $date <br/> Nombre: $firstname <br/> Apellido: $lastname <br/> Email: $email <br/> Numero: $phone";
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/plain; charset=iso-8859-1" . "\r\n";
+	$headers .= "De: ".$firstname." ".$lastname." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
+	$msg     = "
+	Servicio: $service,
+	Staff: $staff,
+	Fecha solicitada: $date, 
+	Nombre: $firstname, 
+	Apellido: $lastname, 
+	Email: $email , 
+	Numero: $phone";
 	
    $mail =  mail($to, $email_subject, $msg, $headers);
-  if($mail)
+if($mail)
 	{
 		echo 'success';
 	}
